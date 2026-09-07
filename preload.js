a9a5686338523dfefe098e5575bd8800962eeffa
YHalo <285117859@qq.com>
2026-09-07 22:02:55 +0800

    feat: expose firmware analyzer IPC

--- preload.js
+++ preload.js
@@ -6,5 +6,7 @@ contextBridge.exposeInMainWorld('newcyber', {
   inspectFile: (rootPath, relativePath) => ipcRenderer.invoke('workspace:inspect', rootPath, relativePath),
   saveReport: (payload) => ipcRenderer.invoke('report:save', payload),
   saveArtifact: (artifact) => ipcRenderer.invoke('artifact:save', artifact),
+  chooseAndAnalyzeFirmware: () => ipcRenderer.invoke('firmware:choose-analyze'),
+  extractFirmwareWithBinwalk: (filePath) => ipcRenderer.invoke('firmware:extract-binwalk', filePath),
   runTool: (tool, payload) => ipcRenderer.invoke('toolbox:run', tool, payload)
 });
