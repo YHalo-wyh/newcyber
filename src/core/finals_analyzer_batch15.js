8d4eb8304b443d055baf121cef0740ce1b278fa8
YHalo <285117859@qq.com>
2026-09-09 09:06:47 +0800

    test: preserve Batch20 compatibility marker while routing Batch21

--- src/core/finals_analyzer_batch15.js
+++ src/core/finals_analyzer_batch15.js
@@ -1,2 +1,3 @@
 // Compatibility entrypoint: Electron/main and older imports automatically receive the newest workspace analyzer.
+// Previous compatibility target: finals_analyzer_batch20.
 module.exports = require('./finals_analyzer_batch21');
