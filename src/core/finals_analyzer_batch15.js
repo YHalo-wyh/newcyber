2255773fb8fa9cedca430a94555d954620355ddf
YHalo <285117859@qq.com>
2026-09-09 09:42:32 +0800

    chore: advance compatibility entrypoint to Batch22

--- src/core/finals_analyzer_batch15.js
+++ src/core/finals_analyzer_batch15.js
@@ -1,3 +1,2 @@
 // Compatibility entrypoint: Electron/main and older imports automatically receive the newest workspace analyzer.
-// Previous compatibility target: finals_analyzer_batch20.
-module.exports = require('./finals_analyzer_batch21');
+module.exports = require('./finals_analyzer_batch22');
