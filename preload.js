530d9f2236833a07dbeae7e82f8d7fb11d383d17
YHalo <285117859@qq.com>
2026-09-11 23:23:57 +0800

    Batch89 expose artifact reveal action to renderer

--- preload.js
+++ preload.js
@@ -16,6 +16,7 @@ contextBridge.exposeInMainWorld('newcyber', {
   },
   rescanChallenge: (rootPath) => ipcRenderer.invoke('challenge:rescan', rootPath),
   inspectChallengeFile: (rootPath, relativePath) => ipcRenderer.invoke('challenge:inspect', rootPath, relativePath),
+  revealArtifact: (rootPath, relativePath) => ipcRenderer.invoke('artifact:reveal-path', rootPath, relativePath),
   getPocIndexStatus: () => ipcRenderer.invoke('poc:index-status'),
   importPocIndex: () => ipcRenderer.invoke('poc:index-import'),
   getAdvisoryIndexStatus: () => ipcRenderer.invoke('advisory:index-status'),
