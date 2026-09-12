27b08729c6a36ef7ece481efc649069ea722373d
YHalo <285117859@qq.com>
2026-09-12 12:23:46 +0800

    Batch108: remove temporary scheduler probe

--- tests/batch108-scheduler-probe.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const test=require('node:test');
-const {getTrainingCurriculum}=require('../src/core/ai_training_curriculum');
-test('Batch108 scheduler probe',()=>{
-  const c=getTrainingCurriculum();
-  const probe={queue:(c.schedule?.queue||[]).slice(0,3),orders:(c.workOrders?.orders||[]).slice(0,3).map((o)=>({id:o.id,direction:o.direction,priority:o.priority,score:o.score,objective:o.objective,acquisition:o.acquisition}))};
-  throw new Error('BATCH108_PROBE='+JSON.stringify(probe));
-});
